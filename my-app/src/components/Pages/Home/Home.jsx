e1cc070208a2cc7561da6a36b3c785115a3a91a9
ArizdelcyLizbeth <arizdelcylizbeth1203@ciencias.unam.mx>
2024-04-14 21:19:53 -0600

    Actualizando Home

--- my-app/src/components/Pages/Home/Home.jsx
+++ my-app/src/components/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import './Home.css';
 
 export function Home() {
   return (
-    <div className='cont'>
-      <div className='container'>
-        <h1 className='title'>Bienvenido a ClonBuster</h1>
-        <img src='https://png.pngtree.com/png-clipart/20220303/original/pngtree-cartoon-cute-pink-round-background-film-projector-film-png-image_7386187.png'/>
+    <div className='home-container'>
+      <div className='content'>
+        <h1 className='title'>¡Bienvenido a ClonBuster!</h1>
+        <p className='description'>Explora nuestro catálogo de películas y disfruta de la mejor experiencia cinematográfica.</p>
+        <div className='image-container'>
+          <img src='https://png.pngtree.com/png-clipart/20220303/original/pngtree-cartoon-cute-pink-round-background-film-projector-film-png-image_7386187.png' alt='Proyector de cine' className='image' />
+        </div>
       </div>
     </div>
   );

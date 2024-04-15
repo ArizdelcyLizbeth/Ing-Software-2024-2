b838256161967b02b5039291f4d24cf52c74c267
ArizdelcyLizbeth <arizdelcylizbeth1203@ciencias.unam.mx>
2024-04-14 21:07:56 -0600

    Modificando

--- my-app/src/components/Root/Navigation/Navigation.jsx
+++ my-app/src/components/Root/Navigation/Navigation.jsx
@@ -9,19 +9,19 @@ export default function Navigation() {
         <nav>
           <ul>
             <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink to='/'>Inicio</NavLink>
               <img src="https://png.pngtree.com/png-clipart/20220303/original/pngtree-cartoon-cute-pink-round-background-film-projector-film-png-image_7386187.png" alt="Im" />
             </li>
             <li>
-              <NavLink to='movies'>Movies</NavLink>
+              <NavLink to='movies'>Películas</NavLink>
               <img src="https://jaimefariasinforma.com/wp-content/uploads/2023/06/Diseno-sin-titulo-97.jpg" alt="Movies Image" />
             </li>
             <li>
-              <NavLink to='users'>Users</NavLink>
+              <NavLink to='users'>Usuarios</NavLink>
               <img src="https://images.vexels.com/media/users/3/135250/isolated/preview/f476f930e98bea4034f47b9dd713d128-icono-de-sombra-de-usuarios.png" alt="Users Image" />
             </li>
             <li>
-              <NavLink to='rents'>Rents</NavLink>
+              <NavLink to='rents'>Rentas</NavLink>
               <img src="https://i.pinimg.com/originals/42/97/83/429783b5e349774bee8bcd4f8f11e14e.png" alt="Rents Image" />
             </li>
           </ul>
